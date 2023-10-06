3cf093f886d7fafd2b7892ba4ab879046096d96c
daniel <dramirezn18@gmail.com>
2023-10-06 12:05:23 -0500

    ciclo for integrado en la conversion de grados

--- capitulo1/2_conversion_fah_cel.c
+++ capitulo1/2_conversion_fah_cel.c
@@ -10,29 +10,13 @@
 // imprime la tabla de F-C para fahr=0,20,...,300
 int main() {
     // declaracion de variables
-    float fahr, celcius;
-    int lower, upper, step;
-    // asignacion de variables
-    lower = 0;
-    upper = 300;
-    step = 20;
+    int fahr;
     /*
-        iteracion: bucle while (mientras)
-        condicon: si los grados fahr son menor o igual a upper, se cumple. Si no se cumple
+        iteracion: bucle for
+        condicon: si los grados fahr son menor o igual a 300 se seguira iterando
         el ciclo termina
-    */ 
-    fahr = lower;
-    while (fahr <= upper) {
-        // operación aritmetica: el resultado se almacenara en celcius
-        celcius = (5.0/9.0) * (fahr-32);
-        /*
-            %f: indica un argumento flotante
-            %3.0f -> tendra 3 caracteres de ancho, sin punto decimal y sin digitos fraccionarios
-            %6.1f -> tendra 6 caracteres de ancho, con un digito despues del punto decimal
-
-            Ejemplos: %6f, %.2f, %f
-        */
-        printf("%3.0f\t%6.1f\n", fahr, celcius); 
-        fahr = fahr + step; // Se le sumara step:20 a fahr y se asignara a fahr
+    */
+    for (fahr=0; fahr <= 300; fahr += 20) {
+        printf("%3.0f\t%6.1f\n", fahr, (5.0/9.0) * (fahr-32));
     }
 }
